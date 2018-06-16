b7eaa56cf08eeeb8b836a18ddf63399aa72ceb64
Eric Goebelbecker <eric@ericgoebelbecker.com>
2018-06-16 16:13:44 -0400

    Initial commit

---
+++ src/main/java/com/aspetraining/teamcity/TeamCity.java
@@ -0,0 +1,16 @@
+package com.aspetraining.teamcity;
+
+public class TeamCity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+    public boolean getStatus() {
+        return true;
+    }
+
+
+
+}
---
+++ src/test/java/com/aspetraining/teamcity/TeamCityTest.java
@@ -0,0 +1,16 @@
+package com.aspetraining.teamcity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TeamCityTest {
+
+    private TeamCity teamCity = new TeamCity();
+
+
+    @Test
+    public void getStatus() {
+        assertTrue(teamCity.getStatus());
+    }
+}
